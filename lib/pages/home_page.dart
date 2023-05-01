22b61eee680d485ce21d7c8ea5a051564ed5b271
Djibril <djibriltamou@gmail.com>
2023-05-01 23:08:11 +0100

    creation du file get_user

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connexion/read_data/get_user_name.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -12,22 +14,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  //document IDs
+  List<String> docIDs = [];
+
+  //get IDs
+  Future getDocId() async {
+    await FirebaseFirestore.instance.collection('users').get().then(
+        (snapshot) => snapshot.docs.forEach(
+          (document) {
+            print(document.reference);
+            docIDs.add(document.reference.id);
+          }
+        )
+    );
+  }
+
+  /*  @override
+  void initState() {
+    getDocId();
+    super.initState();
+  }*/
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          user.email!,
+        style: TextStyle(fontSize: 16),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: Icon(Icons.logout),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('signed in as ' + user.email!),
-              //prefixIcon: Icons(Icons.singnout),
               /*MaterialButton(
                 onPressed: () {
                   FirebaseAuth.instance.signOut();
                 },
                 color: Colors.deepPurple[200],
                 child: Text('sign out'),
-              ),*/
+              ),
               IconButton(
                 onPressed: () {
                   FirebaseAuth.instance.signOut();
@@ -37,6 +72,25 @@ class _HomePageState extends State<HomePage> {
                 iconSize: 30,
                 icon: Icon(Icons.logout),
                 tooltip: 'Sign Out',
+              ),*/
+              Expanded(
+                child: FutureBuilder(
+                  future: getDocId(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: docIDs.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: GetUserName(documentId: docIDs[index]),
+                            tileColor: Colors.deepPurple[100],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
             ]
         ),
